01171a05b885a2e302dca331bc724f4f4c7371fe
slgeay <avomailbox-github@yahoo.fr>
2024-02-12 12:37:06 +0100

    Refactor fizzbuzzmatch.rs to use tuple directly in match expression

--- src/fizzbuzzmatch.rs
+++ src/fizzbuzzmatch.rs
@@ -1,6 +1,5 @@
 fn fizzbuzz(i: u32) -> String {
-    let r = (i % 3, i % 5);
-    match r {
+    match (i % 3, i % 5) {
         (0, 0) => "FizzBuzz".to_string(),
         (0, _) => "Fizz".to_string(),
         (_, 0) => "Buzz".to_string(),
