22f8a3b4fca91793e418f9be6605a2e2168de6f3
slgeay <avomailbox-github@yahoo.fr>
2024-02-12 10:56:17 +0100

    Init

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let random_number = generate_random_number();
+    let mut my_choice = 10;
+    my_choice += random_number;
+    println!("{my_choice}");
+}
+ 
+fn generate_random_number() -> i32 {
+    4 // chosen by dice roll, guaranteed to be random
+}
