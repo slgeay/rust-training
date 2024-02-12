ee4f849f6636b353ab943f78f0e5c31bea73c61b
slgeay <avomailbox-github@yahoo.fr>
2024-02-12 12:39:30 +0100

    Add FizzBuzz tests to fizzbuzz.rs and fizzbuzzmatch.rs

--- src/fizzbuzz.rs
+++ src/fizzbuzz.rs
@@ -16,3 +16,11 @@ pub(crate) fn main() {
         println!("{}", fizzbuzz(i));
     }
 }
+
+ #[test]
+ fn test_fizzbuzz(){
+    assert_eq!(fizzbuzz(1), "1");
+    assert_eq!(fizzbuzz(3), "Fizz");
+    assert_eq!(fizzbuzz(5), "Buzz");
+    assert_eq!(fizzbuzz(15), "FizzBuzz");
+ }
