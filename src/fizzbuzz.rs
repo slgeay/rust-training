e9722de89243ebd3259cb1e8499755fb8d9c9bb8
slgeay <avomailbox-github@yahoo.fr>
2024-02-12 12:45:42 +0100

    Fix formatting in FizzBuzz and FizzBuzzMatch modules

--- src/fizzbuzz.rs
+++ src/fizzbuzz.rs
@@ -1,9 +1,9 @@
 fn fizzbuzz(i: u32) -> String {
     if i % 15 == 0 {
         String::from("FizzBuzz")
-    } else if i%3 == 0 {
+    } else if i % 3 == 0 {
         String::from("Fizz")
-    } else if i%5 == 0 {
+    } else if i % 5 == 0 {
         String::from("Buzz")
     } else {
         i.to_string()
@@ -17,10 +17,10 @@ pub(crate) fn main() {
     }
 }
 
- #[test]
- fn test_fizzbuzz(){
+#[test]
+fn test_fizzbuzz() {
     assert_eq!(fizzbuzz(1), "1");
     assert_eq!(fizzbuzz(3), "Fizz");
     assert_eq!(fizzbuzz(5), "Buzz");
     assert_eq!(fizzbuzz(15), "FizzBuzz");
- }
+}
--- src/fizzbuzzmatch.rs
+++ src/fizzbuzzmatch.rs
@@ -3,7 +3,7 @@ fn fizzbuzz(i: u32) -> String {
         (0, 0) => "FizzBuzz".to_string(),
         (0, _) => "Fizz".to_string(),
         (_, 0) => "Buzz".to_string(),
-        (_, _) => i.to_string()
+        (_, _) => i.to_string(),
     }
 }
 
@@ -15,9 +15,9 @@ pub(crate) fn main() {
 }
 
 #[test]
-fn test_fizzbuzz(){
-   assert_eq!(fizzbuzz(1), "1");
-   assert_eq!(fizzbuzz(3), "Fizz");
-   assert_eq!(fizzbuzz(5), "Buzz");
-   assert_eq!(fizzbuzz(15), "FizzBuzz");
+fn test_fizzbuzz() {
+    assert_eq!(fizzbuzz(1), "1");
+    assert_eq!(fizzbuzz(3), "Fizz");
+    assert_eq!(fizzbuzz(5), "Buzz");
+    assert_eq!(fizzbuzz(15), "FizzBuzz");
 }
