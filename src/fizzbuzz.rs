f71b0f4178ae78fc9306b74aab97aec651cd217b
slgeay <avomailbox-github@yahoo.fr>
2024-02-13 07:45:26 +0100

    Refactor FizzBuzz to use match expression

--- src/fizzbuzz.rs
+++ src/fizzbuzz.rs
@@ -10,10 +10,23 @@ fn fizzbuzz(i: u32) -> String {
     }
 }
 
+fn fizzbuzz_match(i: u32) -> String {
+    match (i % 3, i % 5) {
+        (0, 0) => "FizzBuzz".to_string(),
+        (0, _) => "Fizz".to_string(),
+        (_, 0) => "Buzz".to_string(),
+        (_, _) => i.to_string(),
+    }
+}
+
 pub(crate) fn main() {
     println!("<<< FizzBuzz >>>");
     for i in 1..=100 {
         println!("{}", fizzbuzz(i));
+    }
+    println!("<<< FizzBuzz Match >>>");
+    for i in 1..=100 {
+        println!("{}", fizzbuzz_match(i));
     }
 }
 
@@ -23,4 +36,12 @@ fn test_fizzbuzz() {
     assert_eq!(fizzbuzz(3), "Fizz");
     assert_eq!(fizzbuzz(5), "Buzz");
     assert_eq!(fizzbuzz(15), "FizzBuzz");
+}
+
+#[test]
+fn test_fizzbuzz_match() {
+    assert_eq!(fizzbuzz_match(1), "1");
+    assert_eq!(fizzbuzz_match(3), "Fizz");
+    assert_eq!(fizzbuzz_match(5), "Buzz");
+    assert_eq!(fizzbuzz_match(15), "FizzBuzz");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 mod fizzbuzz;
-mod fizzbuzz_match;
 mod narcissistic_number_check;
 mod rustlatin;
 
 fn main() {
     fizzbuzz::main();
-    fizzbuzz_match::main();
     rustlatin::main();
     narcissistic_number_check::main();
 }
