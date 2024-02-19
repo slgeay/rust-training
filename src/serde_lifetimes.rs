ff44a5013e9be50aabdb45e600d012eab5e040be
slgeay <avomailbox-github@yahoo.fr>
2024-02-19 12:47:15 +0100

    Implement serde lifetimes Step 1

---
+++ src/serde_lifetimes.rs
@@ -0,0 +1,48 @@
+use serde_json::{json, Value};
+
+// pretend that we call an API and get a JSON String back
+fn fetch_data() -> String {
+    String::from(
+        r#"
+            {
+                "id": 1,
+                "title": "Hello, Rust"
+            }
+        "#,
+    )
+}
+
+#[derive(Debug)]
+struct BlogPost {
+    id: u32,
+
+    title: String,
+}
+
+pub fn main() -> Result<(), serde_json::Error> {
+    println!("<<< Serde Lifetimes >>>");
+    let post: BlogPost = {
+        let data = fetch_data();
+        let v: Value = serde_json::from_str(&data)?;
+        BlogPost { 
+            id: v["id"].as_u64().unwrap() as u32, 
+            title: v["title"].as_str().unwrap().to_string()
+        }
+    };
+    println!("deserialized = {:?}", post);
+
+    let post_json: String =
+        serde_json::to_string(
+            &json!(
+                {
+                    "id": post.id,
+                    "title": post.title
+                }
+            )
+        )?
+    ;
+
+    println!("serialized = {:?}", post_json);
+
+    Ok(())
+}
