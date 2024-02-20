784f299bac250363ad1bbfffc4fbc429e21741ed
slgeay <avomailbox-github@yahoo.fr>
2024-02-20 12:57:41 +0100

    Refactor TCP server to use a message passing channel for logging

--- src/tcp_server.rs
+++ src/tcp_server.rs
@@ -1,8 +1,8 @@
 // https://github.com/ferrous-systems/rust-exercises/blob/main/exercise-book/src/tcp-server.md
 
-use std::{io::{self, BufRead, BufReader, Write}, net::{TcpListener, TcpStream}, sync::Mutex, thread};
+use std::{io::{self, BufRead, BufReader, Write}, net::{TcpListener, TcpStream}, sync::mpsc::{self, Sender}, thread};
 
-fn handle_client(mut stream: TcpStream, log: &Mutex<Vec<usize>>) -> Result<(), io::Error> {
+fn handle_client(mut stream: TcpStream, log: Sender<usize>) -> Result<(), io::Error> {
     let mut buffer = String::new();
     let mut reader = BufReader::new(stream.try_clone()?); 
     loop {
@@ -11,8 +11,7 @@ fn handle_client(mut stream: TcpStream, log: &Mutex<Vec<usize>>) -> Result<(), i
             Ok(0) => return Ok(()),
             Ok(_) => {
                 {
-                    let mut log = log.lock().expect("Could not lock log");
-                    log.push(buffer.len());
+                    log.send(buffer.len()).unwrap();
                 }
                 write!(stream, "{}", buffer)?;
             }
@@ -26,13 +25,23 @@ fn handle_client(mut stream: TcpStream, log: &Mutex<Vec<usize>>) -> Result<(), i
 pub fn main() -> Result<(), io::Error> {
     println!("<<< TCP Server >>>");
 
-    let log = Mutex::new(vec![]);
+    let mut log = vec![];
+
+    let (sender, receiver) = mpsc::channel();
+
     let listener = TcpListener::bind("127.0.0.1:7878")?;
     thread::scope(|s| {
+        s.spawn(move || {
+            while let Ok(size) = receiver.recv() {
+                log.push(size);
+                println!("{:?}", log);
+            }
+        });
         loop {
             let (stream, _) = listener.accept().unwrap();
+            let sender = sender.clone();
             s.spawn({|| {
-                    if let Err(e) = handle_client(stream, &log) {
+                    if let Err(e) = handle_client(stream, sender) {
                         eprintln!("Error: {}", e);
                     }
                 }
